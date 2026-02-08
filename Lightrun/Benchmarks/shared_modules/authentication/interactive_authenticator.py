37528b6f5bacf21915deacab8c910e9e72e08825
CannoNadav <Cannonadav@gmail.com>
2026-02-08 11:08:21 +0200

    small refactor + adding header

--- Lightrun/Benchmarks/shared_modules/authentication/interactive_authenticator.py
+++ Lightrun/Benchmarks/shared_modules/authentication/interactive_authenticator.py
@@ -10,6 +10,11 @@ from .credentials import Credentials
 class InteractiveAuthenticator(Authenticator):
     """Authenticates using the Device Authorization Flow (OAuth 2.0)."""
 
+    DEFAULT_HEADERS = {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+
     def __init__(self, api_url: str, company_id: str, logger: logging.Logger):
         self.api_url = api_url
         self.company_id = company_id
@@ -17,22 +22,15 @@ class InteractiveAuthenticator(Authenticator):
         self.session = requests.Session()
         self._credentials = Credentials(logger, api_url, company_id)
 
-    def get_headers(self) -> Dict[str, str]:
+    def get_headers(self, **kwargs) -> Dict[str, str]:
+        headers = {**InteractiveAuthenticator.DEFAULT_HEADERS}
         auth_token = self._credentials.get_access_token()
-        if not auth_token:
-            return {}
-        return {
-            "Authorization": f"Bearer {auth_token}",
-            "Content-Type": "application/json",
-        }
-
-    def send_authenticated_request(self, session: requests.Session, method: str, url: str, **kwargs) -> requests.Response:
-        auth_token = self._credentials.get_access_token()
-
-        # Prepare headers
-        headers = kwargs.pop('headers', {})
-        headers["Content-Type"] = "application/json"
         if auth_token:
             headers["Authorization"] = f"Bearer {auth_token}"
 
+        override_headers = kwargs.pop('override_headers', {})
+        return {**headers, **override_headers}
+
+    def send_authenticated_request(self, session: requests.Session, method: str, url: str, **kwargs) -> requests.Response:
+        headers = self.get_headers(**kwargs)
         return session.request(method, url, headers=headers, **kwargs)
